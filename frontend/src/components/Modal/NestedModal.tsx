58029d66593a089bce0e917631edd24f9e310125
ViniciusBrJp <vgccarmona@gmail.com>
2024-10-26 12:12:28 +0900

    名前入力欄

--- frontend/src/components/Modal/NestedModal.tsx
+++ frontend/src/components/Modal/NestedModal.tsx
@@ -1,23 +1,9 @@
-// NestedModal.tsx
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
-import styles from "./NestedModal.module.scss";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  pt: 2,
-  px: 4,
-  pb: 3,
-};
+import TextField from "@mui/material/TextField";
+import styles from "./NestedModal.module.scss"; // スタイルモジュールをインポート
 
 interface NestedModalProps {
   setModalClosed: React.Dispatch<React.SetStateAction<boolean>>;
@@ -25,25 +11,36 @@ interface NestedModalProps {
 
 export default function NestedModal({ setModalClosed }: NestedModalProps) {
   const [open, setOpen] = React.useState(true);
+  const [name, setName] = React.useState(""); // 名前の状態を追加
+
   const handleClose = () => {
     setOpen(false);
     setModalClosed((prev) => !prev);
+  };
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
   };
 
   return (
     <div>
       <Modal
         open={open}
-        onClose={handleClose}
         aria-labelledby="parent-modal-title"
         aria-describedby="parent-modal-description"
       >
-        <Box sx={{ ...style, width: 400 }}>
-          <h2 id="parent-modal-title">Text in a modal</h2>
-          <p id="parent-modal-description">
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </p>
-          <Button onClick={handleClose}>完了</Button>
+        <Box className={styles.modalBox}>
+          <h2 id="parent-modal-title">Enter NickName</h2>
+          <TextField
+            label="nickname"
+            value={name}
+            onChange={handleNameChange}
+            fullWidth
+            margin="normal"
+          />
+          <Button className={styles.modalButton} onClick={handleClose}>
+            送信
+          </Button>
         </Box>
       </Modal>
     </div>
