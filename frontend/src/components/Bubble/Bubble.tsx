f9863e7422a7039bbf07f2c0eab8512e1c4065d2
MurakawaTakuya <murakawatakuya5@gmail.com>
2024-10-26 18:04:50 +0900

    名前に表示するハッシュの長さを4文字に変更

--- frontend/src/components/Bubble/Bubble.tsx
+++ frontend/src/components/Bubble/Bubble.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { MessageInterface } from "@/types/interface";
+import styles from "./Bubble.module.scss";
 
 export default function Bubble({ bubble }: { bubble: MessageInterface }) {
   return (
     <p>
-      {bubble.senderName} @{bubble.addressHash}: {bubble.message}
+      <span className={styles.name}>{bubble.senderName}</span>
+      <span className={styles.hash}>
+        #{bubble.addressHash.slice(0, 4)}
+      </span>: {bubble.message}
     </p>
   );
 }
