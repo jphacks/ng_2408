a2a33236492ac475c722448b2951b61308000e98
MurakawaTakuya <murakawatakuya5@gmail.com>
2024-10-27 11:50:26 +0900

    バブルデザイン変更

--- frontend/src/components/Bubble/Bubble.tsx
+++ frontend/src/components/Bubble/Bubble.tsx
@@ -8,11 +8,40 @@ export default function Bubble({
   bubble: messageDownEventInterface;
 }) {
   return (
-    <p>
-      <span className={styles.name}>{bubble.name}</span>
-      <span className={styles.hash}>
-        #{bubble.addressHash.slice(0, 4)}
-      </span>: <span className={styles.message}>{bubble.message}</span>
-    </p>
+    <div className={styles.bubbleContainer}>
+      {bubble.isSelfMessage ? (
+        <>
+          <div className={styles.userData} style={{ textAlign: "right" }}>
+            <span className={styles.name}>{bubble.name}</span>
+            <span className={styles.hash}>
+              #{bubble.addressHash.slice(0, 4)}
+            </span>
+          </div>
+          <div className={styles.bubble}>
+            <div className={styles.chat}>
+              <div className={styles.myMessage}>
+                <p>{bubble.message}</p>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className={styles.userData} style={{ textAlign: "left" }}>
+            <span className={styles.name}>{bubble.name}</span>
+            <span className={styles.hash}>
+              #{bubble.addressHash.slice(0, 4)}
+            </span>
+          </div>
+          <div className={styles.bubble}>
+            <div className={styles.chat}>
+              <div className={styles.message}>
+                <p>{bubble.message}</p>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
